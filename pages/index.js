30a94cb27f263cefbe6323dd9d2956c1fd8f81ac
Ian A. Escobar <ianaragonescobar@gmail.com>
2025-12-15 20:43:31 +0000

    modifica frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Kim, se você leu essa mensagem manda uma bandeira do Brasil no zap</h1>
+    return <h1>Kim, se você leu essa mensagem manda uma bandeira do Brasil no zap ⚡</h1>
 }
 
 export default Home;
